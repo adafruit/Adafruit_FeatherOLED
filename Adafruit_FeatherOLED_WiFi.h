975a021e5b9e6b883bc638a0aab70874125de7a2
Nithin Shenoy <nithin_shenoy@hotmail.com>
2017-04-02 22:43:14 -0400

    Adds RSSI WiFi Bar Icon
    Adds ability to display WiFi signal strength as a bar icon instead of
    renderng the "RSSI" text label.

--- Adafruit_FeatherOLED_WiFi.h
+++ Adafruit_FeatherOLED_WiFi.h
@@ -44,16 +44,21 @@
 
 class Adafruit_FeatherOLED_WiFi : public Adafruit_FeatherOLED
 {
+  private: 
+    int rssiToQualityPercentage ( void );
+
   protected:
     bool    _connected;
     bool    _connectedVisible;
     int     _rssi;
+    bool    _rssiIcon;
     bool    _rssiAsPercentage;
     bool    _rssiVisible;
     int32_t _ipAddress;
     bool    _ipAddressVisible;
 
     void renderRSSI       ( void );
+    void renderRSSIIcon   ( void );
     void renderConnected  ( void );
     void renderIPAddress  ( void );
 
@@ -65,6 +70,7 @@ class Adafruit_FeatherOLED_WiFi : public Adafruit_FeatherOLED
       _connected          = false;
       _connectedVisible   = true;
       _rssi               = -99;
+      _rssiIcon           = false;
       _rssiAsPercentage   = false;
       _rssiVisible        = true;
       _ipAddress          = 0;
@@ -74,6 +80,7 @@ class Adafruit_FeatherOLED_WiFi : public Adafruit_FeatherOLED
     void setConnected        ( bool conn )      { _connected = conn; }
     void setConnectedVisible ( bool enable )    { _connectedVisible = enable; }
     void setRSSI             ( int rssi )       { _rssi = rssi; }
+    void setRSSIIcon         ( bool enable )    { _rssiIcon = enable; }
     void setRSSIAsPercentage ( bool enable )    { _rssiAsPercentage = enable; }
     void setRSSIVisible      ( bool enable )    { _rssiVisible = enable; }
     void setIPAddress        ( uint32_t addr )  { _ipAddress = addr; }
