665afdc31f8ebf70d4b7a1984fb14b6c5b07c320
lady ada <limor@ladyada.net>
2021-10-26 19:34:48 -0400

    fix nrf52

--- Adafruit_FeatherOLED.h
+++ Adafruit_FeatherOLED.h
@@ -50,19 +50,19 @@
   #define VBATPIN PA1
   #define VBAT_MULTIPLIER  2.0 * 0.80566F / 1000.0
 
-#elif defined(ARDUINO_FEATHER52)
+#elif defined(ARDUINO_FEATHER52) || defined(NRF52840_XXAA)
   #define VBATPIN PIN_VBAT
   #define VBAT_MV_PER_LSB   (0.73242188F)   // 3.0V ADC range and 12-bit ADC resolution = 3000mV/4096
   
   #ifdef NRF52840_XXAA    // if this is for nrf52840
     #define VBAT_DIVIDER      (0.5F)          // 150K + 150K voltage divider on VBAT
-    #define VBAT_DIVIDER_COMP (2.0F)          // Compensation factor for the VBAT divider
+    #define VBAT_MULTIPLIER (2.0F)          // Compensation factor for the VBAT divider
   #else
     #define VBAT_DIVIDER      (0.71275837F)   // 2M + 0.806M voltage divider on VBAT = (2M / (0.806M + 2M))
-    #define VBAT_DIVIDER_COMP (1.403F)        // Compensation factor for the VBAT divider
+    #define VBAT_MULTIPLIER (1.403F)        // Compensation factor for the VBAT divider
   #endif
 
-  #define REAL_VBAT_MV_PER_LSB (VBAT_DIVIDER_COMP * VBAT_MV_PER_LSB)
+  #define REAL_VBAT_MV_PER_LSB (VBAT_MULTIPLIER * VBAT_MV_PER_LSB)
 
 #elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
   #include "Adafruit_LC709203F.h"
