a22585fc922efb59e608cf099d5987b75f311722
lady ada <limor@ladyada.net>
2021-10-26 20:17:14 -0400

    manuclank

--- Adafruit_FeatherOLED_SH110X.h
+++ Adafruit_FeatherOLED_SH110X.h
@@ -85,12 +85,13 @@ public:
   void renderBattery(void);
 };
 
-#define BATTTEXT_STARTX 77 ///< Battery Text X Position
-#define BATTTEXT_STARTY 0 ///< Battery Text Y Position
+#define BATTTEXT_STARTX 77  ///< Battery Text X Position
+#define BATTTEXT_STARTY 0   ///< Battery Text Y Position
 #define BATTICON_STARTX 110 ///< Battery Icon X Position
-#define BATTICON_STARTY 0 ///< Battery Icon X Position
-#define BATTICON_WIDTH 18 ///< Battery Icon Width
+#define BATTICON_STARTY 0   ///< Battery Icon X Position
+#define BATTICON_WIDTH 18   ///< Battery Icon Width
 #define BATTICON_BARWIDTH3                                                     \
   ((BATTICON_WIDTH - 6) / 3) ///< Battery Icon Bar Width
 
 #endif /* _Adafruit_FeatherOLED_SH110X_H_ */
+ 
